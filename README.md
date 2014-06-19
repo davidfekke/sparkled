e3179b104a50a34d555033a2c5aa2af9f2790386
David Fekke <david@fekke.com>
2014-06-18 20:48:26 -0400

    First commit for Sparkled

---
+++ README.md
@@ -0,0 +1,5 @@
+# Turn on my Led Sparkled
+
+This project is an example for the JaxNode user group. This is a one of the demos for the June IoT presentation.
+
+Copyright David Fekke, 2014
---
+++ routes/led.js
@@ -0,0 +1,79 @@
+
+var https = require('https'),
+	querystring = require('querystring'),
+	body = "",
+	returnValue = "",
+	accessToken = process.env.SPARKACCESSTOKEN; //process.env.SPARKACCESSTOKEN
+
+var httpsOptions = {
+	hostname: 'api.spark.io',
+	port: 443,
+	path: '/v1/devices/' + process.env.ZEDDEVICE + '/led', //process.env.ZEDDEVICE
+	method: 'POST',
+	headers: {
+		'Accept': '*/*',
+		'Accept-Language': 'en-US,en;q=0.8',
+		'Accept-Encoding': 'gzip,deflate,sdch',
+		'Content-Type': 'application/x-www-form-urlencoded'
+	}
+};
+
+function callSparkService(postData, res) {
+	var post_data = postData;
+	httpsOptions.headers['Content-Length'] = post_data.length;
+	var retValue = "";
+	var request = https.request(httpsOptions, function(response) {
+		console.log('STATUS: ' + response.statusCode);
+		response.setEncoding('utf8');
+		response.on('data', function (chunk) {
+			//console.log('BODY: ' + chunk);
+			retValue += chunk;
+		});
+		response.on('end', function() {
+			console.log('request has ended.');
+			//console.log(body);
+			//retValue = JSON.parse(body);
+			res.send(retValue);
+		});
+	});
+	
+	request.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+
+	// write data to request body
+	request.write(post_data);
+	request.end();
+}
+
+exports.redledon = function (req, res) {
+	var post_data = querystring.stringify({
+		'access_token': accessToken,
+		'params': 'D7,HIGH'
+	});
+	callSparkService(post_data, res);
+};
+
+exports.redledoff = function (req, res) {
+	var post_data = querystring.stringify({
+		'access_token': accessToken,
+		'params': 'D7,LOW'
+	});
+	callSparkService(post_data, res);
+};
+
+exports.greenledon = function (req, res) {
+	var post_data = querystring.stringify({
+		'access_token': accessToken,
+		'params': 'A7,HIGH'
+	});
+	callSparkService(post_data, res);
+};
+
+exports.greenledoff = function (req, res) {
+	var post_data = querystring.stringify({
+		'access_token': accessToken,
+		'params': 'A7,LOW'
+	});
+	callSparkService(post_data, res);
+};
